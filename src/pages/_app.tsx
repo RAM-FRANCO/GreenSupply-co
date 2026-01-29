eb2d8c15fa29e6e5c20620343c59bda3917063dd
Ram Franco <ramchristianfranco2@gmail.com>
2026-01-29 09:01:05 +0800

    config: add localization provider for date pickers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,21 @@ import theme from "@/theme/theme";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import QueryProvider from "@/components/providers/QueryProvider";
 import ChartRegistry from "@/components/dashboard/ChartRegistry";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryProvider>
       <ChartRegistry />
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <DashboardLayout>
-          <Component {...pageProps} />
-        </DashboardLayout>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <DashboardLayout>
+            <Component {...pageProps} />
+          </DashboardLayout>
+        </ThemeProvider>
+      </LocalizationProvider>
     </QueryProvider>
   );
 }
